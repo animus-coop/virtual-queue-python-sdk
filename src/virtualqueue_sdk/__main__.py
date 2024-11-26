227c9f2a956ec20ec8671f30ed6bfe6edb3a2e8a
Nicolás Miñán <nm.imagenysonido@gmail.com>
2024-11-26 13:10:07 -0300

    main: print token verification result

--- src/virtualqueue_sdk/__main__.py
+++ src/virtualqueue_sdk/__main__.py
@@ -3,15 +3,18 @@ from virtualqueue_sdk import queues
 
 
 def _print_welcome():
+    print("\n")
     print("Thanks for using Virtual Queue's Python SDK!")
     print("See https://github.com/animus-coop/virtual-queue-python-sdk for more information 😃")
+    print("\n\n")
 
 
 def main():
 
     if len(sys.argv) > 1:
         token = sys.argv[1]
-        queues.verify_finished_line(token)
+        verification_result = queues.verify_finished_line(token)
+        print(verification_result)
         return
 
     _print_welcome()
