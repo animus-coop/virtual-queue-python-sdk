c63182bbe007c0806f65a4912d0d2b9462c363ee
Michel Romero <studentenherz@gmail.com>
2025-07-14 17:58:33 -0300

    Add some modifications to the verification

--- src/vqueue/queues.py
+++ src/vqueue/queues.py
@@ -1,18 +1,21 @@
 from dataclasses import dataclass
 from datetime import datetime
+from pathlib import Path
 from typing import Optional
+from uuid import UUID
 
 import requests
 
 from ._config import API_BASE_PATH
 
-QUEUES_API_URL = f"{API_BASE_PATH}queue/"
+QUEUES_API_URL = Path(API_BASE_PATH).joinpath("queue/")
+VERIFY_API_URL = QUEUES_API_URL.joinpath("verify")
 
 
 @dataclass
 class VerificationResultData:
     token: str
-    started_at: datetime
+    ingressed_at: datetime
     finished_at: datetime
 
 
@@ -23,22 +26,28 @@ class VerificationResult:
     data: Optional[VerificationResultData] = None
 
 
-def verify_finished_line(token: str) -> VerificationResult:
-    response = requests.get(f"{QUEUES_API_URL}verify?token={token}")
-    if response.status_code == 400:
-        return VerificationResult(success=False, message="Please double-check input token.")
+def verify_token(token: str) -> VerificationResult:
+    try:
+        uuid_token = UUID(token, version=4)
+    except ValueError:
+        return VerificationResult(success=False, message="Invalid UUID")
+
+    response = requests.get(f"{VERIFY_API_URL}?token={uuid_token}")
 
     response_data = response.json()
 
-    if response.status_code == 404:
+    if 200 <= response.status_code < 300:
+        return VerificationResult(
+            success=True,
+            message=response_data["message"],
+            data=VerificationResultData(
+                token=response_data["data"]["token"],
+                ingressed_at=response_data["data"]["finished_line"]["ingressed_at"],
+                finished_at=response_data["data"]["finished_line"]["finished_at"],
+            ),
+        )
+
+    if 400 <= response.status_code < 500:
         return VerificationResult(success=False, message=response_data["message"])
 
-    return VerificationResult(
-        success=True,
-        message=response_data["message"],
-        data=VerificationResultData(
-            token=response_data["data"]["token"],
-            started_at=response_data["data"]["finished_line"]["ingressed_at"],
-            finished_at=response_data["data"]["finished_line"]["finished_at"],
-        ),
-    )
+    return VerificationResult(success=False, message="Please double check your token")
